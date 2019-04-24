449bd63ee2bd57aa18fa67de3ed754a640901792
peng <1048834827@qq.com>
2019-04-24 16:36:38 +0800

    333

--- cloud-feign-one/src/main/java/cn/peng/feigntwo/config/ServiceAConfiguration.java
+++ cloud-feign-one/src/main/java/cn/peng/feigntwo/config/ServiceAConfiguration.java
@@ -4,8 +4,8 @@ package cn.peng.feigntwo.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-@RefreshScope
+//@RefreshScope
 public class ServiceAConfiguration {
-    @Value("${abc.msg}")
-    private String msg;
+//    @Value("${abc.msg}")
+//    private String msg;
 }
