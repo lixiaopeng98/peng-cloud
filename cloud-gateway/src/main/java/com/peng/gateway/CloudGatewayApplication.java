eab71fd8ef502d4b69a5c2605602613e229aa409
peng <1048834827@qq.com>
2019-04-28 18:33:58 +0800

    123

--- cloud-gateway/src/main/java/com/peng/gateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/peng/gateway/CloudGatewayApplication.java
@@ -1,9 +1,11 @@
 package com.peng.gateway;
 
+import fileter.MyFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,4 +16,8 @@ public class CloudGatewayApplication {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
+	@Bean
+	public MyFilter myFilter(){
+		return new  MyFilter();
+	}
 }
---
+++ cloud-gateway/src/main/java/fileter/MyFilter.java
@@ -0,0 +1,78 @@
+package fileter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ObjectUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+/**
+ * 自定义Filter 获取请求头参数  get put 的参数
+ * @author Lxp
+ */
+public class MyFilter extends ZuulFilter {
+
+    private static Logger logger = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+//        Standard types in Zuul are "pre" for pre-routing filtering,
+//        "route" for routing to an origin, "post" for post-routing filters, "error" for error handling.
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        logger.info("进入 MyFilter ");
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+
+        String s = "MyFilter";
+
+        //循环打印请求头信息
+        Enumeration<String> headerNames = request.getHeaderNames();
+        if (!ObjectUtils.isEmpty( headerNames)){
+            while (headerNames.hasMoreElements()){
+                String headerName = headerNames.nextElement();
+                logger.info("{} -- request header : name {}, value {}",s,headerName,request.getHeader(headerName));
+            }
+        }else {
+            logger.info("{} -- request header : name {}, value {}",s,null,null);
+        }
+
+        Enumeration<String> parameterNames = request.getParameterNames();
+        if (!ObjectUtils.isEmpty( parameterNames)){
+            while (parameterNames.hasMoreElements()){
+                String parameterElement = parameterNames.nextElement();
+                logger.info("{} -- request Parameter : name {}, value {}",s,parameterElement,request.getParameter(parameterElement));
+            }
+        }else {
+            logger.info("{} -- request header : name {}, value {}",s,null,null);
+        }
+
+
+        Enumeration<String> attributeNames = request.getAttributeNames();
+        if (!ObjectUtils.isEmpty( attributeNames)){
+            while (attributeNames.hasMoreElements()){
+                String attributeElement = attributeNames.nextElement();
+                logger.info("{} -- request attribute : name {}, value {}",s,attributeElement,request.getParameter(attributeElement));
+            }
+        }else {
+            logger.info("{} -- request attribute : name {}, value {}",s,null,null);
+        }
+
+        return null;
+    }
+}
